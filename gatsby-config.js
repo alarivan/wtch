9912578a47eb7870bbd26c2bee68cad6bbda4180
alarivan <alarivan@pm.me>
2019-09-29 12:33:22 -0700

    Add gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/wtch",
   siteMetadata: {
     title: `wtch`,
     description: `Kick off your next, great Gatsby project with this starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
