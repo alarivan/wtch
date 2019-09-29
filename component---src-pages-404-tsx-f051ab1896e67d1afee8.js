7051d6a3300733fd1bceea33fd89a00d02e25d23
alarivan <alarivan@pm.me>
2019-09-29 12:35:27 -0700

    Updates

---
+++ component---src-pages-404-tsx-f051ab1896e67d1afee8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{238:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(237),o=n(236);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-f051ab1896e67d1afee8.js.map
--- src/components/styled/theme.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  colors: {
-    primary: "#333",
-    secondary: "#444",
-    overlay: "rgba(0, 0, 0, 0.7)",
-  },
-}
