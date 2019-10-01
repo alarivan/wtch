4654c19aa91700ac4b066c14dcbfb2eb4f209893
alarivan <alarivan@pm.me>
2019-10-01 15:09:49 -0700

    Refactor subscribe form

--- src/components/footer/SubscribeForm.tsx
+++ src/components/footer/SubscribeForm.tsx
@@ -1,55 +1,40 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons"
-import styled, { css } from "styled-components"
+import styled from "styled-components"
 
 const Form = styled.form`
-  width: 100%;
-  display: inline-block;
-  background-color: #555;
   position: relative;
-  border-radius: 20px;
-  line-height: 0;
   margin-top: 1rem;
 `
 
-const FormControl = css`
-  display: inline-block;
-  border: 0;
+const Input = styled.input`
+  background-color: #555;
   outline: 0;
   font-size: 1rem;
   color: #ddd;
-  background-color: transparent;
-  font-family: inherit;
-  margin: 0;
-  padding: 0 3rem 0 1.5rem;
-  width: 100%;
-  height: 45px;
-  border-radius: 20px;
-`
-
-const Input = styled.input`
-  ${FormControl}
+  padding: 0 1rem;
+  border: 0;
+  border-radius: 20px 0 0 20px;
 `
 
 const Submit = styled.button`
-  ${FormControl}
-  display: inline-block;
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 45px;
-  height: 45px;
   background-color: #eee;
-  font-size: 1rem;
-  text-align: center;
-  margin: 0;
-  padding: 0;
   outline: 0;
   border: 0;
   color: ${({ theme }) => theme.colors.primary};
   cursor: pointer;
   border-radius: 0 20px 20px 0;
+  padding: 0.5rem 1rem;
+`
+const InputGroup = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Label = styled.label`
+  font-size: 0.9rem;
+  padding: 0 1rem;
 `
 
 interface Props {}
@@ -60,14 +45,13 @@ const SubscribeFrom: React.FC<Props> = () => {
       <h3>Subscribe</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       <Form name="email-form" method="POST" data-netlify="true">
-        <div>
-          <span>
-            <Input type="email" name="email" id="email" placeholder="E-mail" />
-          </span>
-          <Submit type="submit" value="Submit">
+        <Label htmlFor="email">Email</Label>
+        <InputGroup>
+          <Input type="email" name="email" id="email" placeholder="Email" />
+          <Submit type="submit" value="Submit" title="Subscribe">
             <FontAwesomeIcon size="2x" icon={faChevronRight} />
           </Submit>
-        </div>
+        </InputGroup>
       </Form>
     </>
   )
